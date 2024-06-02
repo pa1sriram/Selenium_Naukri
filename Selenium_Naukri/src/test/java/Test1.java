59118eef1b42b168a6f9fec9c6a6eedddee6d3cc
INDIA <INDIA@DESKTOP-DCLC66P>
2024-06-02 23:08:33 +0530

    Day 5 done some validations on login and profile updated status

--- Selenium_Naukri/src/test/java/Test1.java
+++ Selenium_Naukri/src/test/java/Test1.java
@@ -9,20 +9,31 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Test1 {
 
 	public static void main(String[] args) throws InterruptedException, AWTException {
-		//Creating driver instance
-		ChromeDriver driver= new ChromeDriver();
+		// Disable bookmarks bar using Chrome options
+		ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-bookmarks-bar");
+        
+	    //Creating driver instance
+	    ChromeDriver driver= new ChromeDriver(options);
+        
 		//Implicit wait wait which applicable for all web elements
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		//Maximize window size
 		driver.manage().window().maximize();
 		//driver.manage().window().minimize();
 		
 		//Launch Naukri web portal
 		driver.get("https://www.naukri.com/nlogin/login");
+		
+		System.out.println(driver.getTitle());
+		
 		WebElement username=driver.findElement(By.xpath("//input[@id=\"usernameField\"]"));
 		//Enter user name
 		username.sendKeys("pavansriramjakkampudi@gmail.com");
@@ -32,9 +43,18 @@ public class Test1 {
 		//Select login key to login
 		driver.findElement(By.xpath("//button[text()=\"Login\"]")).click();
 		
-		System.out.println(driver.getTitle());
+		//Explicit Wait
+		WebDriverWait myWait=new WebDriverWait(driver,Duration.ofSeconds(10));
+		myWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='name-wrapper']//div[1]")));
 		
-		
+		System.out.println("logged in as "+driver.findElement(By.xpath("//div[@class='name-wrapper']//div[1]")).getText());
+		if((driver.findElement(By.xpath("//div[@class='name-wrapper']//div[1]")).getText()).equals("Pavan Sri Ram Jakkampudi")) {
+			System.out.println("login successfull");
+			System.out.println("Profile updation date: "+driver.findElement(By.xpath("//div[@class='other-info-wrapper']//p")).getText());
+		}
+		else {
+			System.out.println("login failed");
+		}
 		//View profile
 		driver.findElement(By.xpath("//a[text()=\"View\"]")).click();
 		
