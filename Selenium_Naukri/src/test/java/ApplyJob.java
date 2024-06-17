4762980bd2c1f37f42e2d7c769d6826ab54aeb77
INDIA <INDIA@DESKTOP-DCLC66P>
2024-06-17 23:28:18 +0530

    Day 19 Commit

--- Selenium_Naukri/src/test/java/ApplyJob.java
+++ Selenium_Naukri/src/test/java/ApplyJob.java
@@ -1,23 +1,48 @@
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.io.File;
 import java.time.Duration;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class ApplyJob {
 //	ChromeOptions options;
-	ChromeDriver driver;
-  @Test(priority=1)
+	ChromeDriver driver= new ChromeDriver();
+	WebDriverWait myWait= new WebDriverWait(driver,Duration.ofSeconds(10));
+  @BeforeClass
   public void login() {
-	  driver= new ChromeDriver();
+//	  driver= new ChromeDriver();
 	  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	  driver.manage().window().maximize();
 	  driver.get("https://www.naukri.com/nlogin/login");
 	  Utility.screenShot(driver,"launch");
+	  Utility.screenShot(driver,"launch");
+		WebElement username=driver.findElement(By.xpath("//input[@id=\"usernameField\"]"));
+		//Enter user name
+		username.sendKeys("pavansriramjakkampudi@gmail.com");
+		WebElement password=driver.findElement(By.xpath("//input[@id=\"passwordField\"]"));
+		//Enter Password
+		password.sendKeys("NAUKRIpowerp@1");
+		//Select login key to login
+		driver.findElement(By.xpath("//button[text()=\"Login\"]")).click();
+		driver.findElement(By.xpath("//a[text()=\"View\"]")).click();
   }
   
-  @Test(priority=10)
+  @Test(priority=1)
+  public void udateResume() throws InterruptedException, AWTException{
+	  TestCase1.updateResume(driver,myWait);
+  }
+  
+  @AfterClass
   public void logout(){
 	//Select Burger button for logout option
 	driver.findElement(By.xpath("//img[@alt=\"naukri user profile img\"]")).click();
